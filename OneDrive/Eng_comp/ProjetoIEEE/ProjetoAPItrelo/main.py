b6dcb539c64b61f188c064e8f76044ee233d49c6
JessyBento <154305975+JessyBento@users.noreply.github.com>
2023-12-19 05:22:56 -0300

    Update main.py

--- OneDrive/Eng_comp/ProjetoIEEE/ProjetoAPItrelo/main.py
+++ OneDrive/Eng_comp/ProjetoIEEE/ProjetoAPItrelo/main.py
@@ -1,11 +1,11 @@
 # main.py
-
+from config.py import API_KEY, TOKEN
 from report_generator.report_generator import generate_board_report
 
 if __name__ == "__main__":
     # Substituir com suas credenciais e nome do quadro
-    api_key = 'sua-chave-de-api'
-    token = 'seu-token-de-acesso'
+    api_key = API_KEY
+    token = TOKEN
     board_name = 'NomeDoQuadro'
 
     report_file = generate_board_report(api_key, token, board_name)
